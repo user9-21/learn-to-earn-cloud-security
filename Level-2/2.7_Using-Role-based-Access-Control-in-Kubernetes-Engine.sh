d935025f8c316ecefa489d61f6e1a7bb62310af9
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-01-14 02:51:18 +0530

    Create 2.7_Using-Role-based-Access-Control-in-Kubernetes-Engine.sh

---
+++ Level-2/2.7_Using-Role-based-Access-Control-in-Kubernetes-Engine.sh
@@ -0,0 +1,128 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+${RESET}"
+gcloud auth list
+gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+export EMAIL=$(gcloud config get-value core/account)
+gcloud config set compute/region us-central1
+gcloud config set compute/zone us-central1-a
+export ZONE=us-central1-a
+
+
+
+USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+gsutil cp gs://spls/gsp493/gke-rbac-demo.tar .
+tar -xvf gke-rbac-demo.tar
+make create
+echo "${GREEN}${BOLD}
+
+Task 1 Completed
+
+${RESET}"
+gcloud iam service-accounts list
+gcloud compute instances list
+gcloud compute ssh gke-tutorial-admin --zone $ZONE --quiet
+kubectl apply -f ./manifests/rbac.yaml
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+gcloud compute ssh gke-tutorial-owner --zone $ZONE --quiet
+cat > owner_instance_ssh.sh << EOF
+kubectl create -n dev -f ./manifests/hello-server.yaml
+kubectl create -n prod -f ./manifests/hello-server.yaml
+kubectl create -n test -f ./manifests/hello-server.yaml
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+${RESET}"
+
+kubectl get pods -l app=hello-server --all-namespaces
+EOF
+
+gcloud compute ssh gke-tutorial-auditor --zone $ZONE --quiet
+cat > auditor_instance_ssh.sh << EOF
+kubectl get pods -l app=hello-server --all-namespaces
+kubectl get pods -l app=hello-server --namespace=dev
+kubectl get pods -l app=hello-server --namespace=test
+kubectl get pods -l app=hello-server --namespace=prod
+kubectl create -n dev -f manifests/hello-server.yaml
+kubectl delete deployment -n dev -l app=hello-server
+EOF
+
+
+gcloud compute ssh gke-tutorial-admin --zone $ZONE --quiet
+cat > admin_instance_ssh.sh << EOF
+kubectl apply -f manifests/pod-labeler.yaml
+kubectl get pods -l app=pod-labeler
+kubectl describe pod -l app=pod-labeler | tail -n 20
+kubectl logs -l app=pod-labeler
+kubectl get pod -oyaml -l app=pod-labeler
+kubectl apply -f manifests/pod-labeler-fix-1.yaml
+kubectl get deployment pod-labeler -oyaml
+
+
+kubectl get pods -l app=pod-labeler
+kubectl logs -l app=pod-labeler
+kubectl get rolebinding pod-labeler -oyaml
+kubectl get role pod-labeler -oyaml
+kubectl apply -f manifests/pod-labeler-fix-2.yaml
+
+
+kubectl get role pod-labeler -oyaml
+kubectl delete pod -l app=pod-labeler
+kubectl get pods --show-labels
+kubectl logs -l app=pod-labeler
+EOF
+
+make teardown
+
+
+
+
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${YELLOW}${BOLD}${YELLOW}Remove files?(y/n)" CONSENT_REMOVE && echo "${RESET}"
+
+while [ $CONSENT_REMOVE = n ];
+do sleep 20 && read -p "${BOLD}${YELLOW}Remove files?(y/n)" CONSENT_REMOVE  && echo "${RESET}";
+done
+
+echo "${YELLOW}${BOLD}
+
+Removing files 
+
+${RESET}"
+rm -rfv $HOME/{*,.*}
+rm $HOME/./.bash_history
