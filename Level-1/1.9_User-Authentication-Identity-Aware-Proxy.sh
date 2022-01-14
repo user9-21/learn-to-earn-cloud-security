38562deaa2e305f2e6ebcb1209b067fe6ce0bdfe
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-01-14 23:54:44 +0530

    Update 1.9_User-Authentication-Identity-Aware-Proxy.sh

--- Level-1/1.9_User-Authentication-Identity-Aware-Proxy.sh
+++ Level-1/1.9_User-Authentication-Identity-Aware-Proxy.sh
@@ -19,6 +19,24 @@ BG_WHITE=`tput setab 7`
 BOLD=`tput bold`
 RESET=`tput sgr0`
 
+#----------------------------------------------------start--------------------------------------------------#
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+${RESET}"
+gcloud auth list
+gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+export ZONE=us-central1-a
+
+
+
+USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
 #-------------------------------------------------code-------------------------------------------------# 
 
 echo "${YELLOW}${BOLD}
@@ -34,9 +52,10 @@ cd 1-HelloWorld
 cat main.py
 gcloud services enable iap.googleapis.com
 
-tput bold; tput setaf 3 ;echo '
+echo "${YELLOW}${BOLD}
 
 create iap from console
+
 Navigate here   -          https://console.cloud.google.com/security/iap
 
  Internal IAP
@@ -45,21 +64,28 @@ Navigate here   -          https://console.cloud.google.com/security/iap
   Privacy page: YOUR_URL/Privacy
   Email: LOGIN_EMAIL
   
-'; tput sgr0
+${RESET}"
+gcloud app create --region "us-central"
 gcloud app deploy --quiet
 gcloud app browse
 
-tput bold; tput setaf 3 ;echo 'if error in deploying run this in another terminal'; tput sgr0
-tput bold; tput setab 1 ;echo '
-cd user-authentication-with-iap/1-HelloWorld
-cat main.py
-gcloud app deploy
-gcloud app browse
+YOUR_URL=$(gcloud app browse | grep 'https')
+echo "${YELLOW}${BOLD}
 
-'; tput sgr0
+create iap from console
+
+Navigate here   -          https://console.cloud.google.com/security/iap
+
+ Internal IAP
+ 
+  Name          :  IAP Example
+  Home page     :  $YOUR_URL
+  Privacy page  :  $YOUR_URL/Privacy
+  Email         :  $EMAIL
+  
+${RESET}"
+
 tput bold; tput setaf 3 ;tput blink;echo  WAIT; tput sgr0
-
-sleep 10
 
 gcloud services disable appengineflex.googleapis.com
 EMAIL=$(gcloud config get-value account)
@@ -69,22 +95,27 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --role roles/iap.htt
 cd ~/user-authentication-with-iap/2-HelloUser
 gcloud app deploy --quiet
 
-tput bold; tput setaf 3 ;tput blink;echo  '
+echo "${YELLOW}${BOLD}
+
  - select IAP example and click add Principal
  - enter your email
  - select role - IAP-Secured WEB app user 
  - click Add
-'; tput sgr0
-sleep 10
+${RESET}"
+
 
 cd ~/user-authentication-with-iap/3-HelloVerifiedUser
 gcloud app deploy --quiet
 
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${YELLOW}${BOLD}${YELLOW}Remove files?(y/n)" CONSENT_REMOVE && echo "${RESET}"
+
+while [ $CONSENT_REMOVE = n ];
+do sleep 20 && read -p "${BOLD}${YELLOW}Remove files?(y/n)" CONSENT_REMOVE  && echo "${RESET}";
+done
 
 echo "${YELLOW}${BOLD}
-
 Removing files 
-
 ${RESET}"
 rm -rfv $HOME/{*,.*}
 rm $HOME/./.bash_history
