8ee526993d53e5aeb401a15b3c678b07c28fdad7
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-01-15 00:30:41 +0530

    Update 1.5_Cloud-IDS-Qwik-Start.sh

--- Level-1/1.5_Cloud-IDS-Qwik-Start.sh
+++ Level-1/1.5_Cloud-IDS-Qwik-Start.sh
@@ -19,14 +19,26 @@ BG_WHITE=`tput setab 7`
 BOLD=`tput bold`
 RESET=`tput sgr0`
   
+#----------------------------------------------------start--------------------------------------------------#
+
 echo "${YELLOW}${BOLD}
 
 Starting Execution 
 
 ${RESET}"
-
-export ZONE=us-east1-b
+gcloud auth list
+gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
 export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+export ZONE=us-central1-a
+
+
+
+USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
 
 export PROJECT_ID=$(gcloud config get-value project | sed '2d')
 gcloud services enable servicenetworking.googleapis.com --project=$PROJECT_ID
@@ -145,22 +157,19 @@ sudo touch eicar.file
 echo 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*' | sudo tee eicar.file
 rm *
 exit
+
 EOF
 chmod +x server_ssh.sh
 echo "${CYAN}${BOLD}
 
-File permission granted 
+File permission granted to server_ssh.sh
 
 ${RESET}"
 
 gcloud compute scp --zone=$ZONE --quiet server_ssh.sh server:~
 
-echo "${BLUE}${BOLD}
-
-  server_ssh.sh :↴↴↴
-
-"
-cat server_ssh.sh
+#echo "${BLUE}${BOLD}  server_ssh.sh :↴↴↴"
+#cat server_ssh.sh
 
 echo "${RESET}${BG_RED}${BOLD}
 
@@ -216,23 +225,18 @@ curl http://192.168.10.20/cgi-bin/../../../..//bin/cat%20/etc/passwd
 curl -H 'User-Agent: () { :; }; 123.123.123.123:9999' http://192.168.10.20/cgi-bin/test-critical
 rm *
 exit
+
 EOF
 
 chmod +x attacker_ssh.sh
 echo "${CYAN}${BOLD}
 
-File permission granted 
+File permission granted to attacker_ssh.sh
 
 ${RESET}"
 
 gcloud compute scp --zone=$ZONE --quiet attacker_ssh.sh server:~
 
-echo "${BLUE}${BOLD}
-
-  attacker_ssh.sh :↴↴↴
-
-"
-cat attacker_ssh.sh
 
 echo "${RESET}${BG_RED}${BOLD}
 
@@ -248,10 +252,15 @@ echo "${YELLOW}${BOLD}
 Back IN shell 
 
 ${RESET}"
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${YELLOW}${BOLD}${YELLOW}Remove files?(y/n)" CONSENT_REMOVE && echo "${RESET}"
+
+while [ $CONSENT_REMOVE = n ];
+do sleep 20 && read -p "${BOLD}${YELLOW}Remove files?(y/n)" CONSENT_REMOVE  && echo "${RESET}";
+done
+
 echo "${YELLOW}${BOLD}
-
 Removing files 
-
 ${RESET}"
 rm -rfv $HOME/{*,.*}
 rm $HOME/./.bash_history
