8ab95867f860d9796e6b3b51c685ed5651a4a6e5
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-01-15 16:40:59 +0530

    Update 1.10_Secure-Workloads-in-Google-Kubernetes-Engine-Challenge-Lab.sh

--- Level-1/1.10_Secure-Workloads-in-Google-Kubernetes-Engine-Challenge-Lab.sh
+++ Level-1/1.10_Secure-Workloads-in-Google-Kubernetes-Engine-Challenge-Lab.sh
@@ -33,21 +33,21 @@ export EMAIL=$(gcloud config get-value core/account)
 export ZONE=us-central1-a
 #----------------------------------------------------code--------------------------------------------------#
 echo " "
-read -p "${BOLD}${YELLOW}Enter Cluster name :" CLUSTER_NAME
-read -p "Enter Cloud SQL Instance :" SQL_INSTANCE
-read -p "Enter Service Account :" SERVICE_ACCOUNT
+read -p "${BOLD}${YELLOW}Enter Cluster name : " CLUSTER_NAME
+read -p "Enter Cloud SQL Instance : " SQL_INSTANCE
+read -p "Enter Service Account : " SERVICE_ACCOUNT
 echo "${RESET} "
-echo "${BOLD}${CYAN}Your Cluster name :$CLUSTER_NAME  "
-echo "Your Cloud SQL Instance :$SQL_INSTANCE  "
-echo "Your Service Account :$SERVICE_ACCOUNT  ${RESET}"
+echo "${BOLD}${CYAN}Your Cluster name : $CLUSTER_NAME  "
+echo "Your Cloud SQL Instance : $SQL_INSTANCE  "
+echo "Your Service Account : $SERVICE_ACCOUNT  ${RESET}"
 echo " "
 #read -p "Verify all details are correct?(y/n):" VERIFY_DETAILS
 
-read -p "${BOLD}${YELLOW}Verify all details are correct?(y/n) :" VERIFY_DETAILS && echo "${RESET}"
+read -p "${BOLD}${YELLOW}Verify all details are correct?(y/n) : " VERIFY_DETAILS && echo "${RESET}"
 
 
 while [ $VERIFY_DETAILS = n ];
-do read -p "${BOLD}${YELLOW}Enter Cluster name :" CLUSTER_NAME && read -p "Enter Cloud SQL Instance :" SQL_INSTANCE && read -p "Enter Service Account :" SERVICE_ACCOUNT && echo " " && echo "${BOLD}${CYAN}Your Cluster name :$CLUSTER_NAME" && echo "Your Cloud SQL Instance :$SQL_INSTANCE" && echo "Your Service Account :$SERVICE_ACCOUNT${RESET}" && read -p "${BOLD}${YELLOW}Verify all details are correct?(y/n):" VERIFY_DETAILS && echo "${RESET}" ;
+do read -p "${BOLD}${YELLOW}Enter Cluster name : " CLUSTER_NAME && read -p "Enter Cloud SQL Instance : " SQL_INSTANCE && read -p "Enter Service Account : " SERVICE_ACCOUNT && echo " " && echo "${BOLD}${CYAN}Your Cluster name : $CLUSTER_NAME" && echo "Your Cloud SQL Instance : $SQL_INSTANCE" && echo "Your Service Account : $SERVICE_ACCOUNT${RESET}" && read -p "${BOLD}${YELLOW}Verify all details are correct?(y/n) : " VERIFY_DETAILS && echo "${RESET}" ;
 done
 
 gsutil cp gs://spls/gsp335/gsp335.zip .
@@ -67,6 +67,12 @@ gcloud container clusters create $CLUSTER_NAME \
    --num-nodes 2 \
    --enable-network-policy
    
+echo "${GREEN}${BOLD}
+
+Task 1 Completed
+
+${RESET}"
+   
 gcloud sql instances create $SQL_INSTANCE --region us-central1
 
 
@@ -82,6 +88,18 @@ Create database 'wordpress' in sql instance manually here:- https://console.clou
 ${RESET}"
 gcloud sql users create wordpress --instance $SQL_INSTANCE --host %
 
+read -p "${BOLD}${YELLOW}Created Database 'wordpress' ?(y/n)" CREATE_DATABASE && echo "${RESET}"
+
+while [ $CREATE_DATABASE = n ];
+do sleep 20 && read -p "${BOLD}${YELLOW}Created Database 'wordpress' ?(y/n)" CREATE_DATABASE && echo "${RESET}";
+done
+
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
 gcloud iam service-accounts create $SERVICE_ACCOUNT
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" --role="roles/cloudsql.client"
 gcloud iam service-accounts keys create key.json --iam-account=$SERVICE_ACCOUNT@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
@@ -90,9 +108,18 @@ kubectl create secret generic cloudsql-instance-credentials --from-file key.json
 kubectl create secret generic cloudsql-db-credentials \
    --from-literal username=wordpress \
    --from-literal password=''
-   
+
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+${RESET}"
+
+kubectl create -f volume.yaml
 sed -i "s#INSTANCE_CONNECTION_NAME#$DEVSHELL_PROJECT_ID:us-central1:$SQL_INSTANCE#g" wordpress.yaml
 
+sleep 2
+kubectl apply -f wordpress.yaml
 helm version
 helm repo add stable https://charts.helm.sh/stable
 helm repo update
@@ -106,7 +133,7 @@ do sleep 10 && kubectl get service && read -p "External IP Appeared ? (y/n):" EX
 done
 
 . add_ip.sh  
-read -p "${YELLOW}${BOLD}Your DNS Record(from above command):" DNS_RECORD  && echo "${RESET}"
+read -p "${YELLOW}${BOLD}Your DNS Record(from above command) : " DNS_RECORD  && echo "${RESET}"
 
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.16.0/cert-manager.yaml
 
@@ -122,6 +149,24 @@ kubectl apply -f issuer.yaml
 kubectl apply -f ingress.yaml
 kubectl apply -f network-policy.yaml
 gcloud services enable binaryauthorization.googleapis.com
+
+kubectl create -f volume.yaml
+
+kubectl apply -f wordpress.yaml
+kubectl apply -f issuer.yaml
+kubectl apply -f ingress.yaml
+kubectl apply -f network-policy.yaml
+
+echo "${GREEN}${BOLD}
+
+Task 4 Completed
+
+${RESET}"
+echo "${GREEN}${BOLD}
+
+Task 5 Completed
+
+${RESET}"
 
 echo "${BOLD}${YELLOW}
 
@@ -150,6 +195,21 @@ kubectl apply -f psp-restrictive.yaml
 kubectl apply -f psp-role.yaml
 kubectl apply -f psp-use.yaml
 
+read -p "${BOLD}${YELLOW}Done with Manual step?(y/n)" CONSENT_DONE && echo "${RESET}"
+
+while [ $CONSENT_DONE = n ];
+do sleep 20 && read -p "${BOLD}${YELLOW}Done with Manual step?(y/n)" CONSENT_DONE && echo "${RESET}" ;
+done
+echo "${GREEN}${BOLD}
+
+Task 6 Completed
+
+${RESET}"
+echo "${GREEN}${BOLD}
+
+Task 7 Completed
+
+${RESET}"
 #-----------------------------------------------------end----------------------------------------------------------#
 read -p "${BOLD}${YELLOW}${BOLD}${YELLOW}Remove files?(y/n)" CONSENT_REMOVE && echo "${RESET}"
 
