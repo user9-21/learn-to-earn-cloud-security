bd777456d01715330f170d0b419100e012043e7c
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-01-27 09:00:31 +0530

    Update 2.4_Create-an-Internal-Load-Balancer.sh

--- Level-2/2.4_Create-an-Internal-Load-Balancer.sh
+++ Level-2/2.4_Create-an-Internal-Load-Balancer.sh
@@ -41,26 +41,6 @@ export ZONE=us-central1-a
 
 
 cat > 2.sh << EOF
-gcloud compute instances create utility-vm --zone=us-central1-f --machine-type=f1-micro --network-interface=network-tier=PREMIUM,private-network-ip=10.10.20.50,subnet=subnet-a --maintenance-policy=MIGRATE --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any --create-disk=auto-delete=yes,boot=yes,device-name=utility-vm,image=projects/debian-cloud/global/images/debian-10-buster-v20210916,mode=rw,size=10,type=projects/$GOOGLE_CLOUD_PROJECT/zones/us-central1-a/diskTypes/pd-balanced 
-
-gcloud compute health-checks create tcp my-ilb-health-check --region=us-central1 --port=80
-gcloud compute backend-services create my-ilb --load-balancing-scheme=internal --protocol=tcp --region=us-central1 --health-checks=my-ilb-health-check --health-checks-region=us-central1
-echo "${YELLOW}${BOLD}Confirm load balancer is properly Configured - https://console.cloud.google.com/net-services/loadbalancing/loadBalancers/list?project=$PROJECT_ID ${RESET}"
-
-gcloud compute forwarding-rules create fr-ilb --region=us-central1 --load-balancing-scheme=internal --network=my-internal-app --subnet=subnet-b --address=10.10.30.5 --ip-protocol=TCP --ports=80 --backend-service=my-ilb --backend-service-region=us-central1
-echo "${GREEN}${BOLD}
-
-Task 3 Completed
-
-${RESET}"
-EOF
-chmod +x 2.sh
-echo "${YELLOW}${BOLD}
-Run this in another instance:
-${BG_RED}
-./2.sh
-
-${RESET}"
 gcloud compute firewall-rules create app-allow-http --direction=INGRESS --priority=1000 --network=my-internal-app --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=lb-backend
 gcloud compute firewall-rules create app-allow-health-check --direction=INGRESS --priority=1000  --action=ALLOW --rules=tcp --source-ranges=130.211.0.0/22,35.191.0.0/16 --target-tags=lb-backend
 echo "${YELLOW}${BOLD}
@@ -74,6 +54,32 @@ echo "${GREEN}${BOLD}
 Task 1 Completed
 
 ${RESET}"
+
+
+gcloud compute health-checks create tcp my-ilb-health-check --region=us-central1 --port=80
+gcloud compute backend-services create my-ilb --load-balancing-scheme=internal --protocol=tcp --region=us-central1 --health-checks=my-ilb-health-check --health-checks-region=us-central1
+echo "${YELLOW}${BOLD}Confirm load balancer is properly Configured - https://console.cloud.google.com/net-services/loadbalancing/loadBalancers/list?project=$PROJECT_ID ${RESET}"
+
+gcloud compute forwarding-rules create fr-ilb --region=us-central1 --load-balancing-scheme=internal --network=my-internal-app --subnet=subnet-b --address=10.10.30.5 --ip-protocol=TCP --ports=80 --backend-service=my-ilb --backend-service-region=us-central1
+
+gcloud compute instances create utility-vm --zone=us-central1-f --machine-type=f1-micro --network-interface=network-tier=PREMIUM,private-network-ip=10.10.20.50,subnet=subnet-a --maintenance-policy=MIGRATE --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any --create-disk=auto-delete=yes,boot=yes,device-name=utility-vm,image=projects/debian-cloud/global/images/debian-10-buster-v20210916,mode=rw,size=10,type=projects/$GOOGLE_CLOUD_PROJECT/zones/us-central1-a/diskTypes/pd-balanced 
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+
+
+EOF
+chmod +x 2.sh
+echo "${YELLOW}${BOLD}
+Run this in another instance:
+${BG_RED}
+./2.sh
+
+${RESET}"
+
 
 gcloud beta compute instance-templates create instance-template-1 --machine-type=n1-standard-1 --subnet=projects/$GOOGLE_CLOUD_PROJECT/regions/us-central1/subnetworks/subnet-a --network-tier=PREMIUM --metadata=startup-script-url=gs://cloud-training/gcpnet/ilb/startup.sh --maintenance-policy=MIGRATE --region=us-central1 --tags=lb-backend --boot-disk-device-name=instance-template-1
 gcloud beta compute instance-templates create instance-template-2 --machine-type=n1-standard-1 --subnet=projects/$GOOGLE_CLOUD_PROJECT/regions/us-central1/subnetworks/subnet-b --network-tier=PREMIUM --metadata=startup-script-url=gs://cloud-training/gcpnet/ilb/startup.sh --maintenance-policy=MIGRATE --region=us-central1 --tags=lb-backend --boot-disk-device-name=instance-template-2
@@ -95,11 +101,6 @@ ${RESET}"
 
 
 
-echo "${GREEN}${BOLD}
-
-Task 2 Completed
-
-${RESET}"
 
 
 
@@ -107,7 +108,13 @@ echo "${YELLOW}${BOLD}Confirm load balancer is properly Configured - https://con
 
 gcloud compute backend-services add-backend my-ilb --region=us-central1 --instance-group=instance-group-1 --instance-group-zone=us-central1-a
 gcloud compute backend-services add-backend my-ilb --region=us-central1 --instance-group=instance-group-2 --instance-group-zone=us-central1-b
-    
+
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+${RESET}"
+
 #gcloud compute addresses create my-ilb-ip --region=us-central1 --subnet=subnet-b --addresses=10.10.30.5 --purpose=SHARED_LOADBALANCER_VIP
 
 #-----------------------------------------------------end----------------------------------------------------------#
