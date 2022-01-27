c2948b3b3f3767fd9537acea3a6083dd6d9a23ac
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-01-27 10:17:30 +0530

    Update 1.5_Securing-Google-Cloud-with-CFT-Scorecard.sh

--- Level-1/1.5_Securing-Google-Cloud-with-CFT-Scorecard.sh
+++ Level-1/1.5_Securing-Google-Cloud-with-CFT-Scorecard.sh
@@ -68,67 +68,6 @@ Game Completed.Check score
 ${RESET}"
 
 
-
-curl -o cft https://storage.googleapis.com/cft-cli/latest/cft-linux-amd64
-# make executable
-chmod +x cft
-./cft scorecard --policy-path=policy-library/ --bucket=$CAI_BUCKET_NAME
-# Add a new policy to blacklist the IAM Owner Role
-cat > policy-library/policies/constraints/iam_allowlist_owner.yaml << EOF
-apiVersion: constraints.gatekeeper.sh/v1alpha1
-kind: GCPIAMAllowedBindingsConstraintV3
-metadata:
-  name: allowlist_owner
-  annotations:
-    description: List any users granted Owner
-spec:
-  severity: high
-  match:
-    target: ["organizations/**"]
-    exclude: []
-  parameters:
-    mode: allowlist
-    assetType: cloudresourcemanager.googleapis.com/Project
-    role: roles/owner
-    members:
-    - "serviceAccount:admiral@qwiklabs-services-prod.iam.gserviceaccount.com"
-EOF
-./cft scorecard --policy-path=policy-library/ --bucket=$CAI_BUCKET_NAME
-export USER_ACCOUNT="$(gcloud config get-value core/account)"
-export PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_PROJECT --format="get(projectNumber)")
-# Add a new policy to allowlist the IAM Editor Role
-cat > policy-library/policies/constraints/iam_identify_outside_editors.yaml << EOF
-apiVersion: constraints.gatekeeper.sh/v1alpha1
-kind: GCPIAMAllowedBindingsConstraintV3
-metadata:
-  name: identify_outside_editors
-  annotations:
-    description: list any users outside the organization granted Editor
-spec:
-  severity: high
-  match:
-    target: ["organizations/**"]
-    exclude: []
-  parameters:
-    mode: allowlist
-    assetType: cloudresourcemanager.googleapis.com/Project
-    role: roles/editor
-    members:
-    - "user:$USER_ACCOUNT"
-    - "serviceAccount:**$PROJECT_NUMBER**gserviceaccount.com"
-    - "serviceAccount:$GOOGLE_PROJECT**gserviceaccount.com"
-EOF
-./cft scorecard --policy-path=policy-library/ --bucket=$CAI_BUCKET_NAME
-
-
-
-
-
-
-
-
-
-
 ###############################################################################################
 read -p "${BOLD}${YELLOW}Remove files? [y/n]: ${RESET}" CONSENT_REMOVE
 
