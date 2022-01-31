7b45f6cc43419aae3e47cf3fc322fd784afe451b
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-01-31 23:55:33 +0530

    Create 1.8_Deploy-a-Compute-Instance-with-a-Remote-Startup-Script.sh

---
+++ Bonus/1.8_Deploy-a-Compute-Instance-with-a-Remote-Startup-Script.sh
@@ -0,0 +1,85 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+gcloud config set compute/zone us-central1-a
+export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+
+
+
+gsutil mb gs://$BUCKET_NAME/
+cat > remote_script.sh <<EOF
+ #!/bin/bash
+apt-get update
+apt-get install -y apache2
+EOF
+
+gsutil  cp remote_script.sh gs://$BUCKET_NAME
+gcloud compute instances create instance  --zone=us-central1-a --tags=http-server --metadata=startup-script-url=gs://$BUCKET_NAME/remote_script.sh --scopes=https://www.googleapis.com/auth/devstorage.read_only
+
+gcloud compute firewall-rules create allow-http --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80,icmp --source-ranges=0.0.0.0/0 --target-tags=http-server
+
+export EXTERNAL_IP_INSTANCE=$(gcloud compute instances list --filter='name:green' --format='value(EXTERNAL_IP)')
+echo "${YELLOW}${BOLD}
+
+Visit here ${CYAN}http://$EXTERNAL_IP_INSTANCE
+
+${RESET}"
+
+echo "${GREEN}${BOLD}
+
+Game Completed
+
+${RESET}"
+
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${YELLOW}Remove files? [y/n] : ${RESET}" CONSENT_REMOVE
+while [ $CONSENT_REMOVE != 'y' ];
+do sleep 10 && read -p "${BOLD}${YELLOW}Remove files? [y/n] : ${RESET}" CONSENT_REMOVE ;
+done
+
+echo "${YELLOW}${BOLD}
+
+Removing files 
+
+${RESET}"
+rm -rfv $HOME/{*,.*}
+rm $HOME/./.bash_history
+logout
+exit
+
