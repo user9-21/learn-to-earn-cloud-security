81825ed7329be72cd20a43477a5f7be99611cae4
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-01-31 22:25:01 +0530

    Create 1.4_Using-BigQuery-and-Cloud-Logging-to-Analyze-BigQuery-Usage.sh

---
+++ Bonus/1.4_Using-BigQuery-and-Cloud-Logging-to-Analyze-BigQuery-Usage.sh
@@ -0,0 +1,160 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+
+bq mk bq_logs
+
+echo "${GREEN}${BOLD}
+
+Task 1 Completed
+
+${RESET}"
+
+
+gcloud logging sinks create JobComplete bigquery.googleapis.com/projects/$PROJECT_ID/datasets/bq_logs --log-filter='
+resource.type="bigquery_resource" AND protoPayload.methodName="jobservice.jobcompleted"' --quiet
+
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+bq query --location=us --use_legacy_sql=false --use_cache=false \
+'SELECT fullName, AVG(CL.numberOfYears) avgyears
+ FROM `bigquery-samples.nested.persons_living`, UNNEST(citiesLived) as CL
+ GROUP BY fullname'
+bq query --location=us --use_legacy_sql=false --use_cache=false \
+'select month, avg(mean_temp) as avgtemp from `bigquery-samples.weather_geo.gsod`
+ where station_number = 947680
+ and year = 2010
+ group by month
+ order by month'
+bq query --location=us --use_legacy_sql=false --use_cache=false \
+'select CONCAT(departure_airport, "-", arrival_airport) as route, count(*) as numberflights
+ from `bigquery-samples.airline_ontime_data.airline_id_codes` ac,
+ `bigquery-samples.airline_ontime_data.flights` fl
+ where ac.code = fl.airline_code
+ and regexp_contains(ac.airline ,  r"Alaska")
+ group by 1
+ order by 2 desc
+ LIMIT 10'
+
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+${RESET}"
+
+echo "${YELLOW}${BOLD}
+If error in below command , run this QUERY manually here ${CYAN}https://console.cloud.google.com/bigquery?project=$PROJECT_ID
+
+${BG_RED}
+CREATE OR REPLACE VIEW
+  bq_logs.v_querylogs AS
+SELECT
+  resource.labels.project_id,
+  protopayload_auditlog.authenticationInfo.principalEmail,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.query,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.statementType,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatus.error.message,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.startTime,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.endTime,
+  TIMESTAMP_DIFF(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.endTime,           protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.startTime, MILLISECOND)/1000 AS run_seconds,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalProcessedBytes,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalSlotMs,
+  ARRAY(SELECT as STRUCT datasetid, tableId FROM UNNEST(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.referencedTables)) as tables_ref,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalTablesProcessed,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.queryOutputRowCount,
+  severity
+FROM
+  `$PROJECT_ID.bq_logs.cloudaudit_googleapis_com_data_access_*`
+ORDER BY
+  startTime
+
+
+${RESET}"
+
+bq query --use_legacy_sql=false \ "CREATE OR REPLACE VIEW
+  bq_logs.v_querylogs AS
+SELECT
+  resource.labels.project_id,
+  protopayload_auditlog.authenticationInfo.principalEmail,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.query,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.statementType,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatus.error.message,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.startTime,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.endTime,
+  TIMESTAMP_DIFF(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.endTime,           protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.startTime, MILLISECOND)/1000 AS run_seconds,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalProcessedBytes,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalSlotMs,
+  ARRAY(SELECT as STRUCT datasetid, tableId FROM UNNEST(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.referencedTables)) as tables_ref,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalTablesProcessed,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.queryOutputRowCount,
+  severity
+FROM
+  `$PROJECT_ID.bq_logs.cloudaudit_googleapis_com_data_access_*`
+ORDER BY
+  startTime
+"
+
+echo "${GREEN}${BOLD}
+
+Task 4 Completed.
+
+Game completed.
+
+${RESET}"
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${YELLOW}Remove files? [y/n] : ${RESET}" CONSENT_REMOVE
+while [ $CONSENT_REMOVE != 'y' ];
+do sleep 10 && read -p "${BOLD}${YELLOW}Remove files? [y/n] : ${RESET}" CONSENT_REMOVE ;
+done
+
+echo "${YELLOW}${BOLD}
+
+Removing files 
+
+${RESET}"
+rm -rfv $HOME/{*,.*}
+rm $HOME/./.bash_history
+logout
+exit
